22d0b0d9df2e0091177df9ebda48a78f2f15946e
morgado <morgado@MORGADO-PC>
2017-11-14 02:02:11 -0200

    Add project files.

---
+++ GestorEstoque.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestorEstoque.Web.Startup))]
+namespace GestorEstoque.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
